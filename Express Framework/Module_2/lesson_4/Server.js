efbe90af58a64af4acba9a34a4208f52bb34261e
BrianJantjies <31041337+BrianJantjies@users.noreply.github.com>
2019-01-10 22:06:46 +0200

    Commit

---
+++ Express Framework/Module_2/lesson_4/Server.js
@@ -0,0 +1,12 @@
+//Imports
+const express = require('express')
+//Instantiations
+const app = express()
+
+//Routes
+app.get('/', (req, res) => {
+    res.send({msg:'Hello world'})
+})
+
+//Bootup
+app.listen(3000)
