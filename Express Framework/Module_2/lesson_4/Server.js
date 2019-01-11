9253f7d2b1ddbff71c883b66537c903b25c59ea6
BrianJantjies <31041337+BrianJantjies@users.noreply.github.com>
2019-01-11 18:00:55 +0200

    11/01/2019

--- Express Framework/Module_2/lesson_4/Server.js
+++ Express Framework/Module_2/lesson_4/Server.js
@@ -1,16 +1,41 @@
-//Imports
 const express = require('express')
-//Instantiations
 const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+app.use(bodyParser.json())
+app.use(morgan('dev'))
 
 app.use((req, res, next) => {
-    
+    console.log(`${req.method}: ${req.url}`)
+    next()
 })
+app.use((req, res, next) => {
+    if (req.query.api_key) {
+        next()
+    } else {
+        res.status(401).send({msg: 'Not Authorised'})
+    }
 
-//Routes
+})
 app.get('/', (req, res) => {
     res.send({msg:'Hello world'})
 })
 
-//Bootup
+app.get('/accounts', (req, res, next) => {
+    console.log('accounts inline middleware')
+    next(new Error('oopps'))
+},(req, res) => {
+    res.send({msg:'accounts'})
+})
+
+app.post('/transactions', (req, res) => {
+    console.log(req.body)
+    res.send({msg:'transactions'})
+})
+
+app.use((error, req, res, next) => {
+    console.log('Internal Server Error')
+    res.status(500).send(error)
+})
+
 app.listen(3000)
